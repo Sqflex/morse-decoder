4afd2e894deb0bf388e1de95338ba6b6315b5d01
Sqflex <32853595+Sqflex@users.noreply.github.com>
2019-10-11 13:42:33 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,44 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+var result = '';
+var tmp = '';
+
+for (var i = 0; i < expr.length; i++) {
+    // max length of letter in morse table is 10, so we have a check for i not equal  to 0 
+    if (((i % 10) === 0) && (i !== 0))  {
+        result += MORSE_TABLE[tmp];
+        tmp = '';
+    }
+    // if first letter is 1 and next one is 0 - change to '.'
+    if ((expr[i] === '1') && (expr[i+1] === '0')) { 
+        tmp += '.';
+        i++;
+        continue;
+    }
+    // if first letter is 1 and next one is 1 - change to '-'
+    if ((expr[i] === '1') && (expr[i+1] === '1')) {
+        tmp += '-';
+        i++;
+        continue;
+    }
+    // just skip
+    if ((expr[i] === '0') && (expr[i+1] === 0)) {
+        i++;
+        continue;
+    }
+    // space problem
+    if (expr[i] === '*') {
+        i+=10;
+        result += ' ';
+        continue;
+    }
+
+}
+result += MORSE_TABLE[tmp];
+
+return result;
 }
 
 module.exports = {
